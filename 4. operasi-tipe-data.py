ef18411d20a6272497dd3bbbe90d0415cc51c5a1
nessaaa <nnuranisahh@gmail.com>
2022-06-18 08:27:43 +0700

    Melanjutkan

--- 4. operasi-tipe-data.py
+++ 4. operasi-tipe-data.py
@@ -39,21 +39,61 @@ print(len(tujuh))
 print(tujuh)
 
 # Range
-# range dengan parameter n
+    # range dengan parameter n
 for i in range(5):
     print(i)
-
-# range dengan parameter 2
+    # range dengan parameter 2
 for i in range(1, 11):
     print(i)
-
-# range dengan parameter 3
+    # range dengan parameter 3
 print([_ for _ in range(0,20,5)])
 
 # in dan not in
-# untuk mengetahui sebuah nilai atau objek ada dalam list
+    # untuk mengetahui sebuah nilai atau objek ada dalam list
 kalimat = "Belajar Python di Dicoding sangat menyenangkan"
 print('Dicoding' in kalimat)
 print('tidak' in kalimat)
 print('Dicoding' not in kalimat)
 print('tidak' not in kalimat)
+
+# Memberikan nilai untuk multiple variable
+    # Kodingan biasa
+data = ['shirt', 'white', 'L']
+apparel = data[0]
+color = data[1]
+size = data[2]
+    # Menggunakan list/tupple python
+data = ['shirt', 'white', 'L']  # From List
+apparel, color, size = data
+data = ('shirt', 'white', 'L')  # From Tuple
+apparel, color, size = data
+    # Menukar nilai multi variabel
+apparel, color = 'shirt', 'white'
+apparel, color = color, apparel
+print(apparel)
+print(color)
+
+# Sort
+    # Mengurutkan angka atau huruf dalam list ()
+    # Contoh 1
+angka = [100, 1000, 500, 200, 5]
+angka.sort()
+print(angka)
+    # Contoh 2
+kendaraan = ['motor', 'mobil', 'helikopter', 'pesawat']
+kendaraan.sort()
+print(kendaraan)
+    # contoh 3, urutan terbalik menggunakan reverse
+kendaraan = ['motor', 'mobil', 'helikopter', 'pesawat']
+kendaraan.sort(reverse=True)
+print(kendaraan)
+    # Note................
+    # 1. Metode sort tidak dapat mengurutkan list yang memiliki angka dan string sekaligus di dalamnya
+    # 2. Metode sort menggunakan urutan ASCII, sehingga nilai huruf kapital (uppercase) akan lebih
+        # dahulu dibandingkan huruf kecil (lowercase) ===> Untuk mengatasi kendala ini, Anda dapat
+        # memasukkan keyword str.lower pada parameter. Hal ini akan membuat metode sort menganggap
+        # semua objek menggunakan huruf kecil, tanpa mengubah kondisi asli dari objek tersebut.
+    # Contoh 4
+kendaraan = ['Motor', 'Mobil', 'helikopter', 'pesawat']
+kendaraan.sort(key=str.lower)
+print(kendaraan)
