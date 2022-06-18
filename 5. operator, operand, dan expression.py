0b0a9333e924874ce0c54fab425fa7742adddc39
nessaaa <nnuranisahh@gmail.com>
2022-06-18 15:10:07 +0700

    Cas laptop dulu, nanti lanjut lagi

--- 5. operator, operand, dan expression.py
+++ 5. operator, operand, dan expression.py
@@ -22,3 +22,36 @@ print(5 & 3)
         # menghasilkan 1.
         # Representasi binary 5 adalah 101 dan representasi binary 3 adalah 011. 101 and 011 bernilai 001.
         # 001 dalam desimal adalah 1.
+
+    # | (bit-wise OR)
+        # Menjalankan operasi binary OR pada representasi operand pertama dan kedua.
+print(5 | 3)
+        # menghasilkan 7.
+        # Representasi binary 5 adalah 101 dan representasi binary 3 adalah 011. 101 or 011 bernilai 111.
+        # 111 dalam desimal adalah 7.
+
+    # ^ (bit-wise XOR)
+        # Menjalankan operasi binary XOR pada representasi operand pertama dan kedua.
+print(5 ^ 3)
+        # menghasilkan 6.
+        # Representasi binary 5 adalah 101 dan representasi binary 3 adalah 011. 101 xor 011 bernilai 110.
+        # 110 dalam desimal adalah 6.
+
+    # ~ (bit-wise invert)
+        # Menjalankan operasi binary invert pada representasi operand. Nilai invert dari x adalah -(x+1), menggunakan metode Two’s Complement
+print(~5)
+        # menghasilkan -6
+
+# Operator Perbandingan
+    # < atau operator.lt (less than)
+    # > atau operator.gt (greater than)
+    # <= atau operator.le (less than or equal to)
+    # >= atau operator.ge (greater than or equal to)
+    # != atau operator.ne (not equal to)
+from operator import *
+hijau = 5
+kuning = 10
+print('Kelereng Hijau = {}'.format(hijau))
+print('Kelereng Kuning = {}'.format(kuning))
+for func in (lt, le, eq, ne, ge, gt):
+    print('{}(hijau, kuning): {}'.format(func.__name__, func(hijau, kuning)))
