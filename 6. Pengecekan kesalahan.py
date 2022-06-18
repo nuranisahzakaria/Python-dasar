7d64de02cb99b9cb13805f828fc2c8195548ce2e
nessaaa <nnuranisahh@gmail.com>
2022-06-18 18:44:41 +0700

    Dikit-dikit yang penting paham

---
+++ 6. Pengecekan kesalahan.py
@@ -0,0 +1,28 @@
+# LIBRARY PYTHON PENGECEKAN KESALAHAN (STYLE GUIDE PEP8)
+
+# Lint
+
+# Pycodestyle
+    # Untuk instalasi menggunakan manajer paket pip sebagai berikut : pip install pycodestyle.
+
+# Pylint
+    # Untuk instalasi menggunakan manajer paket pip sebagai berikut : pip install pylint
+
+# Flake8
+    # Untuk instalasi menggunakan manajer paket pip sebagai berikut : pip install flake8
+
+# Black
+    # Black adalah proyek open source yang dikembangkan di repository Python Software Foundation
+    # (PSF) dengan lisensi MIT. Untuk mendapatkan gambaran, versi online (tidak resmi) ada di https://black.now.sh.
+    # Untuk instalasi menggunakan manajer paket pip sebagai berikut: pip install black.
+
+# YAPF (Yet Another Python Formatter)
+    # YAPF adalah proyek open source yang dikembangkan di repository Google, dengan lisensi Apache.
+    # Untuk mendapatkan gambaran, versi online (tidak resmi) ada di https://yapf.now.sh
+    # Untuk instalasi menggunakan manajer paket pip sebagai berikut: pip install yapf.
+
+# Autopep8
+    # Autopep8 adalah proyek open source (berlisensi MIT) yang termasuk paling awal untuk memformat kode, dengan
+    # bantuan lint pycodestyle. Untuk instalasi menggunakan manajer paket pip sebagai berikut: pip install autopep8.
+
+# KALO PAKE PYCHARM UDAH NGGA PERLU PAKE LIBRARY DIATAS LAGI, KARENA UDAH ADA BAWAAN................................
