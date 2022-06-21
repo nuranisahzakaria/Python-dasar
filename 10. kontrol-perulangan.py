951efdecd42fa8920226cfdcd5daae4eca765712
nessaaa <nnuranisahh@gmail.com>
2022-06-21 12:07:14 +0700

    Lupa woilahh, aku buat batas lebih besar dari nol, terus loopingnya n + 2, pas dirun kok ngga berenti, rupanya jadi infinitif loop, hahaha

--- 10. kontrol-perulangan.py
+++ 10. kontrol-perulangan.py
@@ -34,3 +34,28 @@ while baris < jumlah_baris:
         continue  # Saat masuk ke if, maka bagian print * diluar if tidak akan dijalankan, langsung ulang ke while
     print("*", end="")
     bintang = bintang+1
+
+# ELSE SETELAH FOR ...................................
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n/x)
+            break
+
+# ELSE SETELAH WHILE..................................
+# CONTOH 1
+n = 13
+while n > 0:
+    n = n-1
+    if n == 8:
+        break
+    print (n)
+else: print("Loop selesai")
+
+# CONTOH 2
+n = 10
+while n > 0:
+    n = n - 2
+    print(n)
+else:
+    print("Loop selesai")
